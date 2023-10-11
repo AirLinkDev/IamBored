8f5d034e49d3dab28cf9e5f682bb48495e3c464d
AirLinkDev <thomas.trebacz@gmail.com>
2023-10-11 09:53:28 -0700

    added API error client notification and filter selection

--- index.js
+++ index.js
@@ -15,10 +15,8 @@ app.get("/", async (req, res) => {
   try {
     const response = await axios.get("https://bored-api.appbrewery.com/random");
     const result = response.data;
-    const retObj = {
-      retElement : result
-    }
-    console.log("Response Object: "+JSON.stringify(retObj))
+  
+    console.log("Initial API Response Object: "+JSON.stringify(result))
     res.render("index.ejs", { data: result });
   } catch (error) {
     console.error("Failed to make request:", error.message);
@@ -32,10 +30,28 @@ app.post("/", async (req, res) => {
   console.log(req.body);
 
   // Step 2: Play around with the drop downs and see what gets logged.
+  
+  let type = req.body.type;
+  let participants = req.body.participants;
+  let apiQuery="https://bored-api.appbrewery.com/filter?type="+type+"&participants="+participants;
   // Use axios to make an API request to the /filter endpoint. Making
   // sure you're passing both the type and participants queries.
   // Render the index.ejs file with a single *random* activity that comes back
   // from the API request.
+  
+  try {
+    const response = await axios.get(apiQuery);
+    const result = response.data[0];
+
+    console.log("API Response Object: "+JSON.stringify(result));
+
+    res.render("index.ejs", { data: result });
+  } catch (error) {
+    console.error("Failed to make request:", error.message);
+    res.render("index.ejs", {
+      error: "No activities that match your criteria.",
+    });
+  }
   // Step 3: If you get a 404 error (resource not found) from the API request.
   // Pass an error to the index.ejs to tell the user:
   // "No activities that match your criteria."
